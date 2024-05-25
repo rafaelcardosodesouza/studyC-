9e1d26039f4027387bce50ff53d962e2adef3095
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2024-05-25 13:25:17 -0300

    add .gitignore

---
+++ iniciante/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Solicita ao usuário que digite um número
+            Console.WriteLine("Digite um número: ");
+        
+            // Lê a entrada do usuário, converte de string para int e armazena na variável 'numero'
+            int numero = Convert.ToInt32(Console.ReadLine());
+        
+            // Exibe o número digitado pelo usuário
+            Console.WriteLine("O número digitado foi: " + numero);
+        
+            // Captura a tecla pressionada pelo usuário sem exibi-la no console
+            char teclaTeclada = Console.ReadKey(true).KeyChar;
+        
+            // Exibe a tecla que foi pressionada pelo usuário
+            Console.WriteLine("A tecla pressionada foi: " + teclaTeclada);
+        }
+    }
+}
